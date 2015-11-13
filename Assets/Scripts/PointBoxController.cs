ad0fa7e26803840e672a880ba545253b2972bd1b
aixgnav <004935823@coyote.csusb.edu>
2015-11-12 21:51:21 -0800

    Audio and GUI
    Added audio and score to game world

---
+++ Assets/Scripts/PointBoxController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointBoxController : MonoBehaviour {
+
+	GameObject scoreUI;
+	//public AudioClip pointAudio;
+	//AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+		scoreUI = GameObject.FindWithTag ("ScoreText");
+		//audio = GetComponent<AudioSource> ();
+	}
+
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.tag == "Player")
+		{
+			//Play point audio
+			GetComponent<AudioSource>().Play();
+			//Add 1 point to score
+			scoreUI.GetComponent<Score>().ScoreSystem += 1;
+			//Destroy box
+			Destroy(this.gameObject,0.2f);
+
+		}
+	}
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+	public Text scoreText;
+	public int score;
+
+	public int ScoreSystem
+	{
+		get { return this.score; }
+		set 
+		{ 
+			this.score = value; 
+			UpdateScore ();
+		}
+		
+	}
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		UpdateScore ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void UpdateScore()
+	{
+		scoreText.text = "Score: " + score;
+	}
+}
